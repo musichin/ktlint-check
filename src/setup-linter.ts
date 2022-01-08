926209206657c7e0431cb8eac4b58396878b660d
Anton Musichin <musichin@users.noreply.github.com>
2022-01-08 20:58:12 +0100

    refactors tool downloading

--- src/setup-linter.ts
+++ src/setup-linter.ts
@@ -1,26 +1,15 @@
-import * as tc from '@actions/tool-cache';
 import * as core from '@actions/core';
 import * as gh from '@actions/github';
 import * as fs from 'fs';
-import * as path from 'path';
 import {HttpClient} from '@actions/http-client';
 import {Tool} from './types';
+import {getOrDownload} from './tool-provisioner';
 
 const TOOL_NAME = 'ktlint';
 const TOOL_FILENAME = TOOL_NAME;
 
 interface Release {
   tag_name: string; // eslint-disable-line camelcase
-}
-
-function createTool(directory: string, version: string): Tool {
-  return {
-    name: TOOL_NAME,
-    filename: TOOL_FILENAME,
-    version,
-    directory,
-    path: path.join(directory, TOOL_FILENAME),
-  };
 }
 
 function buildDownloadUrl(version: string): string {
@@ -52,26 +41,18 @@ async function determineVersion(version: string): Promise<string> {
   return version;
 }
 
-async function getOrDownload(version: string): Promise<Tool> {
-  const cachedPath = tc.find(TOOL_NAME, version);
-  if (cachedPath) {
-    return createTool(cachedPath, version);
-  }
-
-  const downloadUrl = buildDownloadUrl(version);
-  const downloadedFile = await tc.downloadTool(downloadUrl);
-  const path = await tc.cacheFile(
-    downloadedFile,
-    TOOL_FILENAME,
+async function provision(version: string): Promise<Tool> {
+  return getOrDownload(
     TOOL_NAME,
     version,
+    buildDownloadUrl(version),
+    TOOL_FILENAME,
   );
-  return createTool(path, version);
 }
 
 async function install(version: string): Promise<Tool> {
   const finalVersion = await determineVersion(version);
-  const tool = await getOrDownload(finalVersion);
+  const tool = await provision(finalVersion);
   fs.chmodSync(tool.path, '777');
   core.addPath(tool.directory);
   return tool;
--- src/setup-reporter.ts
+++ src/setup-reporter.ts
@@ -1,45 +1,25 @@
-import * as tc from '@actions/tool-cache';
 import {Tool} from './types';
-import * as path from 'path';
+import {getOrDownload} from './tool-provisioner';
 
 const TOOL_VERSION = '1.1.0';
 const TOOL_NAME = 'ktlint-github-reporter';
 const TOOL_FILENAME = `${TOOL_NAME}.jar`;
 
-function createTool(directory: string, version: string): Tool {
-  return {
-    name: TOOL_NAME,
-    filename: TOOL_FILENAME,
-    version,
-    directory,
-    path: path.join(directory, TOOL_FILENAME),
-  };
-}
-
 function buildDownloadUrl(version: string): string {
   return `https://github.com/musichin/ktlint-github-reporter/releases/download/${version}/${TOOL_FILENAME}`;
 }
 
-async function getOrDownload(version: string): Promise<Tool> {
-  const cachedPath = tc.find(TOOL_NAME, version);
-  if (cachedPath) {
-    return createTool(cachedPath, version);
-  }
-
-  const downloadUrl = buildDownloadUrl(version);
-  const downloadedFile = await tc.downloadTool(downloadUrl);
-  const path = await tc.cacheFile(
-    downloadedFile,
-    TOOL_FILENAME,
+async function provision(version: string): Promise<Tool> {
+  return getOrDownload(
     TOOL_NAME,
     version,
+    buildDownloadUrl(version),
+    TOOL_FILENAME,
   );
-
-  return createTool(path, version);
 }
 
 async function install(): Promise<Tool> {
-  return await getOrDownload(TOOL_VERSION);
+  return await provision(TOOL_VERSION);
 }
 
 export {install};
