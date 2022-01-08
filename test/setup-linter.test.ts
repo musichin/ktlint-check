bf69d38d5f4d002a713305cedcb8c762f90a2968
Anton Musichin <musichin@users.noreply.github.com>
2022-01-08 21:24:49 +0100

    removes possibility to download latest version (#81)

--- test/setup-linter.test.ts
+++ test/setup-linter.test.ts
@@ -1,14 +1,7 @@
 import {expect, it, describe} from '@jest/globals';
-import {install, getLatestVersion} from '../src/setup-linter';
+import {install} from '../src/setup-linter';
 
 describe('#setup-linter', () => {
-  describe('#getLatestVersion', () => {
-    it('successfully', async () => {
-      const downloadUrl = await getLatestVersion();
-      expect(downloadUrl).toBeTruthy();
-    });
-  });
-
   describe('#install', () => {
     it('specific version', async () => {
       const tool = await install('0.43.2');
