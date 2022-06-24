a984ab176ff81a312b3138b68b6d20e698831f1f
Anton Musichin <musichin@users.noreply.github.com>
2022-06-24 23:14:46 +0200

    updates action versions

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   verbose: true,
-  collectCoverage: true,
-  coverageReporters: ["clover"]
 }
