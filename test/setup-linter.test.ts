7f356897362ae955657db04c5b86a92db9090e98
Anton Musichin <musichin@users.noreply.github.com>
2025-07-26 11:58:50 +0200

    Upgrades ktlint to version 1.7.1

--- test/setup-linter.test.ts
+++ test/setup-linter.test.ts
@@ -3,7 +3,7 @@ import {install} from '../src/setup-linter';
 describe('#setup-linter', () => {
   describe('#install', () => {
     test('specific version', async () => {
-      const tool = await install('1.7.0');
+      const tool = await install('1.7.1');
       expect(tool).toBeTruthy();
       expect(tool.name).toStrictEqual('ktlint');
     }, 60_000);
--- test/setup-reporter.test.ts
+++ test/setup-reporter.test.ts
@@ -10,10 +10,10 @@ describe('#setup-reporter', () => {
     }, 60_000);
 
     test('ktlint version >= 0.49.0', async () => {
-      const tool = await install('1.7.0');
+      const tool = await install('1.7.1');
       expect(tool).toBeTruthy();
       expect(tool.name).toStrictEqual('ktlint-github-reporter');
-      expect(tool.version).toStrictEqual('3.3.0');
+      expect(tool.version).toStrictEqual('3.3.1');
     }, 60_000);
   });
 });
